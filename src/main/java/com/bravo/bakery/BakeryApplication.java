36f3c2096c587643809f572aac7e4827cebf9315
diego bravo <diegobravo8702@gmail.com>
2019-11-26 01:29:42 -0500

    Docker
    Features:
    - Create docker file for app
    - Create dockerfile for db
    - Create basic script file for db
    - Create docker-compose file

--- src/main/java/com/bravo/bakery/BakeryApplication.java
+++ src/main/java/com/bravo/bakery/BakeryApplication.java
@@ -1,10 +1,20 @@
 package com.bravo.bakery;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BakeryApplication {
+
+	Logger log = Logger.getLogger(BakeryApplication.class);
+
+	@PostConstruct
+	private void toGreet() {
+		log.info("Welcome to Bakery");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BakeryApplication.class, args);
---
+++ src/main/resources/docker/db/scripts-init/queries.sql
@@ -0,0 +1,16 @@
+/**
+* Database Bakery
+* @autor Diego Armando Bravo Sanabria
+*
+*/
+
+SHOW GRANTS;
+SHOW GRANTS FOR CURRENT_USER;
+SHOW GRANTS FOR CURRENT_USER();
+
+CREATE DATABASE IF NOT EXISTS 'bakery';
+
+USE 'bakery';
+
+select 'HOLA ESTO ES UNA PRUEBA';
+
