6a5bc1954c1125b9fd0dd4671a3e6075403db27c
diego bravo <diegobravo8702@gmail.com>
2019-11-28 01:43:56 -0500

    Profiles Environment
    - Add greeting with name of application
    - Name of application is read from application-ENV.properties
    - Set default name as bakery_default
    - Read and use properties into classes

--- src/main/java/com/bravo/bakery/BakeryApplication.java
+++ src/main/java/com/bravo/bakery/BakeryApplication.java
@@ -3,17 +3,24 @@ package com.bravo.bakery;
 import javax.annotation.PostConstruct;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bravo.bakery.config.AppConf;
+
 @SpringBootApplication
 public class BakeryApplication {
+
+	@Autowired
+	AppConf appConf;
 
 	Logger log = Logger.getLogger(BakeryApplication.class);
 
 	@PostConstruct
 	private void toGreet() {
-		log.info("Welcome to Bakery");
+		String appName = appConf.getConfigValue("spring.application.name");
+		log.info("Hi, I'm " + appName);
 	}
 
 	public static void main(String[] args) {
